4a16c6498392ed8635cf09643153313766c1c98b
Sidirius <sid@sh87.net>
2015-03-17 16:12:13 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,6 @@
 mkdir /var/run/sshd
 /usr/bin/supervisord -c /supervisord.conf
 
-# Setup a password for vnc
-RUN mkdir /.vnc
-RUN x11vnc -storepasswd $passwd ~/.vnc/passwd
-
 while [ 1 ]; do
     /bin/bash
 done

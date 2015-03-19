457fc6e69cb8ac4d4a4ff7ed5be77cb34d4b9dcf
Sidirius <sid@sh87.net>
2015-03-19 08:56:30 +0100

    changed startup file to check if vnc passwd is already set

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mkdir /var/run/sshd
 mkdir /root/.vnc
+if [ ! -f /root/.vnc/passwd ]
+then
 x11vnc -storepasswd $passwd /root/.vnc/passwd
+fi
 /usr/bin/supervisord -c /supervisord.conf
 
 while [ 1 ]; do
